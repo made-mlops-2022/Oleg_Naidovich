b7b5885e58ae44e7959462d442f3676ef1be7347
oleg-nai <olegnai2002@gmail.com>
2022-11-29 19:31:08 +0300

    Update run.sh for downloading data from gdrive, delete datasets from rep

--- online_inference/run.sh
+++ online_inference/run.sh
@@ -8,4 +8,18 @@ then
     export PATH_TO_TRANSFORMER="transformer.pkl"
 fi
 
+if [[ ! -f $PATH_TO_MODEL ]]
+then
+    gdown https://drive.google.com/uc?id=1NUOoinTdKszHYEXblNrjeM-GJGFT88wq --output=$PATH_TO_MODEL
+else
+    echo "model exists"
+fi
+
+if [[ ! -f $PATH_TO_TRANSFORMER ]]
+then
+    gdown https://drive.google.com/uc?id=1jJR1wARI70amf_DW2WG7ER5wPg_dpRg3 --output=$PATH_TO_TRANSFORMER
+else
+    echo "transformer exists"
+fi
+
 uvicorn main:app --reload --host 0.0.0.0 --port 8000
